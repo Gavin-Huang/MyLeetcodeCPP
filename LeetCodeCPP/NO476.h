d62382e508b8bf41020ddf3197a3034ad6bd8105
Gavin-Huang <huangwangyi16@163.com>
2017-03-10 20:15:38 +0800

    NO500 Accepted

--- LeetCodeCPP/NO476.h
+++ LeetCodeCPP/NO476.h
@@ -2,6 +2,8 @@
 #define _Solution
 #include<vector>
 #include   <map> 
+#include <ctype.h>
+#include<iostream>
 using namespace std;
 class Solution {
 public:
@@ -41,6 +43,7 @@ public:
 		}
 		return result;
     }
+
  /*   ///500. Keyboard Row
 	Given a List of words, return the words that can be typed using letters of alphabet on only one row's of American keyboard like the image below.*/
 	
@@ -49,7 +52,8 @@ public:
 
 	//}
 	 vector<string> findWords(vector<string>& words) {
-		 vector<string> result ;
+		 vector<string> result;
+		 int numbers[3];
 		 map<char,int>  my_Map;
 		 my_Map.insert(pair<char,int>('q',0)); 
 		 my_Map.insert(pair<char,int>('w',0));
@@ -57,12 +61,61 @@ public:
 		 my_Map.insert(pair<char,int>('r',0));
 		 my_Map.insert(pair<char,int>('t',0));
 		 my_Map.insert(pair<char,int>('y',0));
-	     my_Map.insert(pair<char,int>('e',0));
-	     my_Map.insert(pair<char,int>('e',0));
-	     my_Map.insert(pair<char,int>('e',0));
-	     my_Map.insert(pair<char,int>('e',0));
-	     my_Map.insert(pair<char,int>('e',0));
+	     my_Map.insert(pair<char,int>('u',0));
+	     my_Map.insert(pair<char,int>('i',0));
+	     my_Map.insert(pair<char,int>('o',0));
+	     my_Map.insert(pair<char,int>('p',0));
+
+	     my_Map.insert(pair<char,int>('a',1));
+		 my_Map.insert(pair<char,int>('s',1));
+		 my_Map.insert(pair<char,int>('d',1));
+		 my_Map.insert(pair<char,int>('f',1));
+		 my_Map.insert(pair<char,int>('g',1));
+		 my_Map.insert(pair<char,int>('g',1));
+		 my_Map.insert(pair<char,int>('h',1));
+		 my_Map.insert(pair<char,int>('j',1));
+
+		  my_Map.insert(pair<char,int>('z',2));
+		  my_Map.insert(pair<char,int>('x',2));
+		  my_Map.insert(pair<char,int>('c',2));
+	      my_Map.insert(pair<char,int>('v',2));
+		  my_Map.insert(pair<char,int>('b',2));
+		  my_Map.insert(pair<char,int>('n',2));
+		  my_Map.insert(pair<char,int>('m',2));
+		  int begain=0;
+		  for (int i = 0; i <words.size() ; i++)
+		  {
+			  if(IsInOneRow(my_Map,words[i]))
+			  {
+				  result.insert(result.end(),words[i]);
+			  }
+		  }
+		  return result;
     }
+	 bool IsInOneRow(map<char,int> &mo,string name)
+	 {
+		 int indexs=-1;
+		 int i =0;
+		 cout<<"begain: "<<endl;
+		 while (name[i++]!='\0')
+		 {   
+	       char g=name[i];
+		   if(isupper(g)) 
+			 g=tolower(g);
+		   cout<<g<<" ";
+		   if(indexs==-1)
+			   indexs=mo.at(g);
+		   else
+		   {
+			   if(indexs!=mo.at(g))
+				   return false;
+		   }
+		   cout<<mo.at(g)<<" ";
+		   i++;
+		 }
+		 return true;
+		 
+	 }
 };
 
 #endif
